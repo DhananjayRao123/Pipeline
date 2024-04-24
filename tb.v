ad1cd7f52cb5fc0f3bfe15cab92c1a0b791c9707
tipsharsha <tipsharsha@gmail.com>
2024-04-25 01:56:27 +0530

    Forwarding and Hazard Detection Unit Working

---
+++ tb.v
@@ -0,0 +1,19 @@
+module tb;
+
+pipeline DUT (clk, rst_n);
+
+reg clk, rst_n;
+
+always #5 clk = ~clk;
+
+initial begin
+
+    $dumpfile("tb_pipe.vcd");
+    $dumpvars;
+    clk = 0;
+    rst_n = 0;
+    #5 rst_n = 1;
+    #100 $finish;
+end
+
+endmodule
