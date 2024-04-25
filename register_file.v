db9995f13747ad820134887b49e0996a9541f400
tipsharsha <tipsharsha@gmail.com>
2024-04-25 22:12:46 +0530

    Register values of Set4 were Initialised

--- register_file.v
+++ register_file.v
@@ -9,15 +9,21 @@ output reg [31:0] A, B;
 reg [31:0] register [0:15]; 
 
 initial begin
-    register[2] = 90;
-    register[1] = 10;
-    register[5] = 32'h000000A0;
-    register[4] = 32'h000000B1;
-    register[7] = 32'hBEFF556A;
-    register[8] = 32'hFFEE7129;
-    register[10] = 32'hFAFF7EF;
-    register[11] = 32'hEBAF735D;
+    // //initialize register file
+    // reg2 = 6 reg4 = 0E311
+    // reg5 = 964EB reg7 = 964DA
+    // reg8 = 113E4 reg10 = 1230B
+
+    register[2] = 32'h00000006;
+    register[4] = 32'h0000E311;
+    register[5] = 32'h000964EB;
+    register[7] = 32'h000964DA;
+    register[8] = 32'h000113E4;
+    register[10] = 32'h0001230B;
     //make others 0
+    register[1] = 0;
+    register[11] = 0;
+    register[14] = 0;
     register[0] = 0;
     register[3] = 0;
     register[6] = 0;
